613ce607e9ace6eb3532c4896680e0046a3f1f39
sibil <sibilsavich@gmail.com>
2023-10-11 19:25:44 +0300

    added next line to lessonOne class

--- src/lesson1/LessonOne.java
+++ src/lesson1/LessonOne.java
@@ -4,5 +4,6 @@ public class LessonOne {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Good morning");
+        System.out.println("How are you");
     }
 }
