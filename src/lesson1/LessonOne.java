bca50e35f5ad3571c8116b9200eafdf6c7dd88d8
sibil <sibilsavich@gmail.com>
2023-10-11 21:50:33 +0300

    added 6 line to lessonOne class

--- src/lesson1/LessonOne.java
+++ src/lesson1/LessonOne.java
@@ -7,5 +7,6 @@ public class LessonOne {
         System.out.println("How are you");
         System.out.println("I am fine");
         System.out.println("New branch added");
+        System.out.println("One more branch");
     }
 }
