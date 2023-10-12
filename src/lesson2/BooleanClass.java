f37a4c043cbfd86dd75beff4c8977c3fd90c66e8
sibil <sibilsavich@gmail.com>
2023-10-12 23:52:32 +0300

    added new 3 Classes to lesson2

---
+++ src/lesson2/BooleanClass.java
@@ -0,0 +1,48 @@
+package lesson2;
+
+public class BooleanClass {
+    public static void main(String[] args) {
+        /*boolean myBoolean = false;
+        boolean myBoolean2 = true;
+        System.out.println(myBoolean);
+        System.out.println(myBoolean2);
+
+        //  ==
+        int x = 10;
+        int y = 13;
+        int z = 10;
+        boolean result = x == z;
+        System.out.println(result);
+
+        // !=
+        System.out.println(y != z);
+
+        //> < >= <=
+        System.out.println("=====================");
+        System.out.println(10 > 10);
+        System.out.println(10 >= 10);
+
+        System.out.println("=====================");
+        System.out.println(true != true);
+        System.out.println(true == false);
+        System.out.println(false == false);
+        System.out.println(false != true);
+        System.out.println(!false);
+        System.out.println(!true);
+
+        System.out.println("=====================");
+        //  || or
+        System.out.println((2 > 5) || (2 < 15) || (23 == 4));
+        System.out.println(true || false || false);
+        System.out.println(false || false);
+
+        // && and
+        boolean example = 19 > 18 && 19 % 2 == 2;
+        System.out.println(example);
+
+        System.out.println("Является ли число четным?wfqfq");
+        int number = 34;
+        System.out.println(number);
+        System.out.println(number % 2 == 0);*/
+    }
+}
---
+++ src/lesson2/Casting.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+public class Casting {
+    public static void main(String[] args) {
+        /*byte myByte = 100;
+        int myInt = myByte;
+        System.out.println(myInt);
+
+        int myInt2 = 130;
+        byte myByte2 = (byte) myInt2;
+        System.out.println(myByte2);
+
+        //char
+        char myChar = 'W';
+        char myChar2 = 'w';
+        System.out.println(myChar);
+
+        int myCharInteger = myChar;
+        int myCharInteger2 = myChar2;
+        System.out.println(myCharInteger);
+        System.out.println(myCharInteger2);*/
+    }
+}
