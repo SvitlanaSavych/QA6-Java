e199f0fd7d87bc451ffecd329a6a68b317df727d
sibil <sibilsavich@gmail.com>
2023-11-01 20:28:43 +0200

    added Homework 5

--- src/lesson7/homework6/Task1.java
+++ src/lesson7/homework6/Task1.java
@@ -1,5 +1,9 @@
 package lesson7.homework6;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 //     1) Пользователь задает размерность двумерного массива с клавиатуры.
@@ -7,5 +11,34 @@ public class Task1 {
 //        Необходимо создать одномерный массив, состоящий из максимальных значений
 //        каждого отдельного массива входящего в двумерный.
 //        Новый полученный массив вывести на экран.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two digits the size of array using Enter:");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println("Size of array:" + "[" + x + "]" + "[" + y + "]");
+
+        int[][] randomArray = new int[x][y];
+        int[] result = new int[x];
+
+        Random random = new Random();
+
+        for (int i = 0; i < randomArray.length; i++) {
+            for (int j = 0; j < randomArray[i].length; j++)
+                randomArray[i][j] = random.nextInt(1000);
+        }
+
+        for (int i = 0; i < randomArray.length; i++) {
+            int max = randomArray[i][0];
+
+            for (int j = 1; j < randomArray[i].length; j++)
+                if (max < randomArray[i][j])
+                    max = randomArray[i][j];
+
+            result[i] = max;
+        }
+        System.out.println(Arrays.deepToString(randomArray));
+        System.out.println("New array");
+        System.out.print(Arrays.toString(result));
     }
 }
--- src/lesson7/homework6/Task2.java
+++ src/lesson7/homework6/Task2.java
@@ -1,5 +1,9 @@
 package lesson7.homework6;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 //        2) Написати програму “стрільба по цілі”.
@@ -17,12 +21,75 @@ public class Task2 {
 //• Завдання повинно бути виконане за допомогою масивів (НЕ використовуйте інтерфейси List, Set, Map).
 //                Приклад виведення в консоль:
 //
-//[0, 1, 2, 3, 4, 5]
-//[1, -, *, -, -, -]
-//[2, -, -, -, -, -]
-//[3, -, *, -, -, -]
-//[4, -, -, *, -, -]
-//[5, -, *, -, -, *]
+//  [0, 1, 2, 3, 4, 5]
+//  [1, -, *, -, -, -]
+//  [2, -, -, -, -, -]
+//  [3, -, *, -, -, -]
+//  [4, -, -, *, -, -]
+//  [5, -, *, -, -, *]
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int targetRow = random.nextInt(5) + 1;
+        int targetCol = random.nextInt(5) + 1;
+
+        char[][] array = {
+                {'0', '1', '2', '3', '4', '5'},
+                {'1', '-', '-', '-', '-', '-'},
+                {'2', '-', '-', '-', '-', '-'},
+                {'3', '-', '-', '-', '-', '-'},
+                {'4', '-', '-', '-', '-', '-'},
+                {'5', '-', '-', '-', '-', '-'}};
+
+        System.out.println("All Set. Get ready to rumble!");
+        System.out.println(targetRow + ":" + targetCol);
+        System.out.println();
+        while (true) {
+            for (int i = 0; i < array.length; i++)
+                System.out.println(Arrays.toString(array[i]));
+
+            int shutRow = 0;
+            int shutCol = 0;
+            while (true) {
+                System.out.println("Input Shut Row  1..5 : ");
+                if (scanner.hasNextInt()) {
+                    shutRow = scanner.nextInt();
+                    if (shutRow < 1 || shutRow > 5)
+                        System.out.println("Try again. Not in range 1..5");
+                    else break;
+                } else {
+                    System.out.println("Try again. Not an integer.");
+                    scanner.nextLine();
+                }
+            }
+
+            while (true) {
+                System.out.println("Input Shut Column  1..5 : ");
+                if (scanner.hasNextInt()) {
+                    shutCol = scanner.nextInt();
+                    if (shutCol < 1 || shutCol > 5)
+                        System.out.println("Try again. Not in range 1..5");
+                    else break;
+                } else {
+                    System.out.println("Try again. Not an integer.");
+                    scanner.nextLine();
+                }
+            }
+
+            if (shutRow == targetRow && shutCol == targetCol) {
+
+                System.out.println("YOU HAVE WON !!!");
+                array[targetRow][targetCol] = 'x';
+                break;
+            } else array[shutRow][shutCol] = '*';
+        }
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++)
+            System.out.println(Arrays.toString(array[i]));
+
+        System.out.println();
+        System.out.println("Game Over, Bye...");
     }
 }
